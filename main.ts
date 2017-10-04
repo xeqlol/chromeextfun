94690994b20911399220a5852fb813c56bf00e45
Dmitrii Nemkov <xeqlol@gmail.com>
2017-10-04 19:04:25 +0500

    init commit bois

---
+++ main.ts
@@ -0,0 +1,4 @@
+/// <reference path="node_modules/@types/chrome/index.d.ts"/>
+
+chrome.browserAction.setBadgeBackgroundColor({ color: [100, 100, 100, 255] });
+chrome.browserAction.setBadgeText({text: '99+'});
