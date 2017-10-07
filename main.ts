33630a84d7c5aa676cdfc614f26a315a6215b2d2
Dmitry Nemkov <xeqlol@gmail.com>
2017-10-07 22:31:21 +0500

    hellow Kappa

--- main.ts
+++ main.ts
@@ -1,24 +1,36 @@
 /// <reference path="node_modules/@types/chrome/index.d.ts"/>
 
+interface Traffic {
+	tabId: number,
+	totralTraffic: number;
+}
+
+var requests: Traffic[] = [];
+
 chrome.browserAction.setBadgeBackgroundColor({color: [100, 100, 100, 225]});
 
-var requests: {[tabId: number]: number} = {};
-chrome.browserAction.setTitle({title: "0 requests in total"});
+chrome.webRequest.onHeadersReceived.addListener((details) => {
+	//if(details.method == "HEAD") {
+	{chrome.tabs.get(details.tabId, (tab) => {
+		if(requests.findIndex(x => x.tabId == tab.id) == -1) {
+			requests.push({tabId: tab.id, totralTraffic: 0});
+		}
+		var index: number = requests.findIndex(x => x.tabId == tab.id);
+		console.log("ADKL:SADKJDSKAL:DKSLA");
+		/*
+		requests[index].totralTraffic += (+details.responseHeaders.find(x => x.name == "Content-Length")[0].value);
+		var sizes: string[] = ["b", "kb", "mb", "gb", "tb"];
+		var order: number = 0;
+		var length: number = requests[index].totralTraffic;
+		while(length >= 1024 && order < sizes.length - 1) {
+			order++;
+			length /= 1024;
+		} 
 
-chrome.webRequest.onBeforeRequest.addListener((details) => {
-	if(details.type.toString() == "xmlhttprequest") {
-		chrome.tabs.get(details.tabId, (tab) => {
-			var totalCount: number = 0;
-			chrome.browserAction.getTitle({tabId: tab.id}, (text) => {totalCount = text.split(' ')[0] != "" ? +text.split(' ')[0] : 0});
-			chrome.browserAction.getBadgeText({tabId: tab.id}, (text) => {
-				var count: number = text != "" ? +text : 0;
-				if(text != "99+" && count < 99) {
-					chrome.browserAction.setBadgeText({tabId: tab.id, text: (++count).toString()});
-				}else{
-					chrome.browserAction.setBadgeText({tabId: tab.id, text: "99+"});chrome.browserAction.setBadgeText({tabId: tab.id, text: "99+"});
-				}
-				chrome.browserAction.setTitle({tabId: tab.id, title: (++totalCount).toString() + " requests in total"})
-			});
-		});
-	}
-}, { urls: ["<all_urls>"]}, ["blocking"]);
+		chrome.browserAction.setBadgeText({tabId: tab.id, text: length + "" + sizes[order]});
+		//chrome.browserAction.setBadgeText({tabId: tab.id, text: "99+"});chrome.browserAction.setBadgeText({tabId: tab.id, text: "99+"});
+		//chrome.browserAction.setTitle({tabId: tab.id, title: (++totalCount).toString() + " requests in total"})
+		*/
+	});
+}
+}, {urls: ["http://*/*"]},["blocking", "responseHeaders"]);
