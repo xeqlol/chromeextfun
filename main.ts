33289bbfdb7a859f20cac3b42f548e6d959689d8
Dmitry Nemkov <xeqlol@gmail.com>
2017-10-09 20:01:20 +0500

    finally, it works

--- main.ts
+++ main.ts
@@ -9,28 +9,33 @@ var requests: Traffic[] = [];
 
 chrome.browserAction.setBadgeBackgroundColor({color: [100, 100, 100, 225]});
 
-chrome.webRequest.onHeadersReceived.addListener((details) => {
-	//if(details.method == "HEAD") {
-	{chrome.tabs.get(details.tabId, (tab) => {
+
+chrome.webRequest.onResponseStarted.addListener((details) => {
+	chrome.tabs.get(details.tabId, (tab) => {
 		if(requests.findIndex(x => x.tabId == tab.id) == -1) {
 			requests.push({tabId: tab.id, totralTraffic: 0});
 		}
 		var index: number = requests.findIndex(x => x.tabId == tab.id);
-		console.log("ADKL:SADKJDSKAL:DKSLA");
-		/*
-		requests[index].totralTraffic += (+details.responseHeaders.find(x => x.name == "Content-Length")[0].value);
-		var sizes: string[] = ["b", "kb", "mb", "gb", "tb"];
-		var order: number = 0;
-		var length: number = requests[index].totralTraffic;
-		while(length >= 1024 && order < sizes.length - 1) {
-			order++;
-			length /= 1024;
-		} 
+		for (var i = 0; i < details.responseHeaders.length; ++i) {
+			if (details.responseHeaders[i].name.toLowerCase() == "content-length") {
 
-		chrome.browserAction.setBadgeText({tabId: tab.id, text: length + "" + sizes[order]});
-		//chrome.browserAction.setBadgeText({tabId: tab.id, text: "99+"});chrome.browserAction.setBadgeText({tabId: tab.id, text: "99+"});
-		//chrome.browserAction.setTitle({tabId: tab.id, title: (++totalCount).toString() + " requests in total"})
-		*/
+				requests[index].totralTraffic += (+details.responseHeaders[i].value);
+				var sizes: string[] = ["b", "k", "m", "g", "t"];
+				var order: number = 0;
+				var length: number = requests[index].totralTraffic;
+				while(length >= 1024 && order < sizes.length - 1) {
+					order++;
+					length /= 1024;
+				} 
+				var formatedLength: number = length < 10 ? roundUp(length, 10) : roundUp(length, 1);
+
+				chrome.browserAction.setBadgeText({tabId: tab.id, text: formatedLength + "" + sizes[order]});
+				chrome.browserAction.setTitle({tabId: tab.id, title: roundUp(length, 100) + sizes[order].toUpperCase() + "b in total"})
+			}
+		}
 	});
+}, {urls: ["<all_urls>"]}, ["responseHeaders"]);
+
+function roundUp(num, precision): number {
+	return Math.ceil(num * precision) / precision
 }
-}, {urls: ["http://*/*"]},["blocking", "responseHeaders"]);
